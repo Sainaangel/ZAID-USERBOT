0878bc3e574cddc6f9acc6c734ec7593e5b5696b
Sainaangel <114768321+Sainaangel@users.noreply.github.com>
2023-01-31 16:30:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ API_HASH = getenv("API_HASH", "24385183c93a98ae4155c25d9f5f64b2") #optional
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "844126468").split()))
 OWNER_ID = int(getenv("OWNER_ID", "5467311248"))
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://pankajsain:pankaj9024@cluster0.fdc33h6.mongodb.net/?retryWrites=true&w=majority")
-BOT_TOKEN = getenv("BOT_TOKEN", "6080251945:AAFPZ-5EA-GpbWG1KuPd0ET2J_ehDzQ1PUs")
+BOT_TOKEN = getenv("BOT_TOKEN", "5897701792:AAG6xBrQUTdMfbrnL6V8mQeWuD2FjEtJ5xI")
 ALIVE_PIC = getenv("ALIVE_PIC", "https://te.legra.ph/file/7e5b79d224638c0d1a38f.jpg")
 ALIVE_TEXT = getenv("ALIVE_TEXT", "⏤͟͞ ＦＬムＳＨ三[🇮🇳]")
 PM_LOGGER = getenv("PM_LOGGER")
@@ -21,7 +21,7 @@ GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
 BRANCH = getenv("BRANCH", "master") #don't change
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "1BJWap1sBu1WDNirSFt2zhK2dhSC6-p2lYSd_wM4uv1vwgUWEliHrqbLBOdMKI0dEm0ZfuXOFFtCB9R1kQSrZSn1aqNiqG-3pD-vljRbJJ8HSYTIHoA-JP0FLJkBZMrmj4juRid2VUG6yaR7m7P_dxcMgCtzghZgeb_AWS8SleFnXFj-73P8Hnv0a2BfaBd9DsfyzaeXvG-QzQNhZGgl_Aceq3v_9sWhBegg10XmdLtmOwy3Wo4OgaCONDeZExE26Vxi40QlZEm0bFGK_3rbSAmogrNl_Sr8qPQA376HNV4sbL62u2wXBPSpRYL-w4E5W55Oi3IiNYk0JKxJHs62z_ZpHfiS_6QI=")
+STRING_SESSION1 = getenv("STRING_SESSION1", "1BJWap1wBu1KSUlK2YZJEaOUM6ZEYYaY4iNSBENXgChOVGbH3YtN2CzSadbAPxNodbZScC9YQwUAQBWKgErdrCBeEa961PYmAK29j9dbTc_zG9BlYNn_FmiGviHMwpbrUXq1BfshhQmpiOFyOusN0wIgb7kldmButI55THxW1UiB8zUc3FgBquQbZtdzxKTmV21PDqd9YqEoIe4cJkMl18avRu3-uD9No_zPFIMOVRXndtiwlLnQBKY0SNt6inkfmxAAS48B0VravI7ITt6N86NAjJWKFfaXrJgdL36GmR2dxwNjnQpy0sNcjzyvUVK003g_GBW3DbmDusKyLN-GHy-S6JqaxAfk=")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
